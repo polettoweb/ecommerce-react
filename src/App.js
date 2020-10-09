e8d1c2f1b3df17d4b7bf1fcb2522f5f123ddf0af
Marco Poletto <marco@surfly.com>
2020-10-09 12:24:31 +0200

    Home split into components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Homepage from './pages/homepage/homepage.component'
 import './App.css';
 
 function App() {
   return (
     <div >
-      Hello World
+      <Homepage />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import Homepage from './homepage.component'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Homepage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
