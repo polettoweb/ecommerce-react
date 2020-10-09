4c0ff2319041ea613ec9daad698c51ae1637f4b0
Marco Poletto <marco@surfly.com>
2020-10-09 15:53:19 +0200

    Added router and menu-item navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component'
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
