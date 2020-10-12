711cc79b877159466992b80256ce2b57b853cf41
Marco Poletto <marco@surfly.com>
2020-10-12 15:35:36 +0200

    Implemented email/pass auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:688879068239:web:71127114eedd3ff10d763a",
     measurementId: "G-RHBGQLND2S"
 }
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log(`error creating user ${error.message}`)
+    }
+  }
+
+  return userRef;
+}
   
 firebase.initializeApp(config)
 
